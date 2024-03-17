515280d47026d05f43be9b2d5745be2f7e34f7fe
fodil benhadjer <109416421+Fodilben@users.noreply.github.com>
2024-03-17 15:00:43 +0000

    adding the contact form

---
+++ src/components/TitleGlobal.jsx
@@ -0,0 +1,8 @@
+const Title = ({ text }) => {
+  return (
+    <h1 className=" text-primary font-semibold  text-[27px] md:text-[40px] xl:text-[80px] leading-[30.47px] md:text-right">
+      {text}
+    </h1>
+  );
+};
+export default Title;
