3e37a4cce3313c279fb532244b444c6e90ddefe4
fodil benhadjer <109416421+Fodilben@users.noreply.github.com>
2024-03-18 16:35:40 +0000

    fix the icons

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,6 +11,17 @@ import {
   goalpc1,
   goalpc2,
   goalpc3,
+  phonePc,
+  emailPc,
+  socialPc,
+  cTikDark,
+  cTikLight,
+  cFaceDark,
+  cFaceLight,
+  cInstDark,
+  cInstLight,
+  cYouDark,
+  cYouLight,
 } from "../assets";
 
 export const navLinks = [
@@ -40,10 +51,30 @@ export const footerLinks2 = [
 ];
 
 export const socialMedia = [
-  { icon: instagram, link: "instagram", text: "إنستغرام" },
-  { icon: facebook, link: "facebook", text: "فيسبوك" },
-  { icon: tiktok, link: "tiktok", text: "تيكتوك" },
-  { icon: youtube, link: "youtube", text: "يوتيوب" },
+  {
+    iconLight: cInstLight,
+    iconDark: cInstDark,
+    link: "instagram",
+    text: "إنستغرام",
+  },
+  {
+    iconLight: cFaceLight,
+    iconDark: cFaceDark,
+    link: "facebook",
+    text: "فيسبوك",
+  },
+  {
+    iconLight: cTikLight,
+    iconDark: cTikDark,
+    link: "tiktok",
+    text: "تيكتوك",
+  },
+  {
+    iconLight: cYouLight,
+    iconDark: cYouDark,
+    link: "youtube",
+    text: "يوتيوب",
+  },
 ];
 
 export const carousel = [
@@ -98,4 +129,3 @@ export const contactInfo = [
     title: "مواقع التواصل",
   },
 ];
-("d");
